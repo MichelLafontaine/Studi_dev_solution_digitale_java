14c23f9646f715aa07496d2970a8ea2f7d146f1a
Michel LAFONTAINE <michel_lafontaine@orange.fr>
2021-10-14 09:42:00 +0200

    creation Spring, dependance Spring Web et mysql driver

---
+++ bibliotheque_evaluation/src/main/java/com/example/bibliotheque_evaluation/BibliothequeEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotheque_evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeEvaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothequeEvaluationApplication.class, args);
+    }
+
+}
